b5590fdfec13fdf31ab793348ff3fb1a39eb2320
Middle44 <4middle4@gmail.com>
2019-11-07 12:02:09 +0100

    v1

---
+++ index.js
@@ -0,0 +1,30 @@
+var Map = ol.Map;
+var View = ol.View;
+var ImageLayer = ol.layer.Image;
+var TileLayer = ol.layer.Tile;
+var ImageWMS = ol.source.ImageWMS;
+var OSM = ol.source.OSM;
+
+var layers = [
+  new TileLayer({
+    source: new OSM()
+  }),
+  new ImageLayer({  //tu treba nastaviť tie veci na svoje vrstvy
+    extent: [-13884991, 2870341, -7455066, 6338219],
+    source: new ImageWMS({
+      url: 'https://ahocevar.com/geoserver/wms', //treba nastaviť na geoserver
+      params: {'LAYERS': 'topp:states'}, //nastaviť správne vrstvy
+      ratio: 1,
+      serverType: 'geoserver'
+    })
+  })
+];
+var map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center: [18, 49], //treba zmeniť súradnice
+    zoom: 4,
+    projection: 'EPSG:4326'
+  })
+});
